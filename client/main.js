b4f23c2b433259c5ea1ada7d60435d236b6f0b6b
andrew.nelsonrd@gmail.com <andrew.nelsonrd@gmail.com>
2017-05-12 11:45:13 -0700

    made headerBar show up

--- client/main.js
+++ client/main.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
-import MainLayout from './layouts/MainLayout';
+import MainLayout from '../imports/ui/layouts/MainLayout';
+import App from '../imports/ui/containers/app/index';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import muiTheme from './styles/mui-theme';
+import muiTheme from './mui-theme';
 
 Meteor.startup(() => {
   ReactDOM.render(
     <MuiThemeProvider muiTheme={muiTheme}>
+      <MainLayout>
+        <App>
+        </App>
+      </MainLayout>
     </MuiThemeProvider>,
     document.getElementById('root')
   )
