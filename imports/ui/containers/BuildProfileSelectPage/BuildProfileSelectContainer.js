b4f711afc873648fab2b0a896daf0e63ee17188e
Evan Gray <evanlawgray@gmail.com>
2017-05-15 13:17:13 -0700

    Change build profile selection button to html button, instead of RaisedButton.

--- imports/ui/containers/BuildProfileSelectPage/BuildProfileSelectContainer.js
+++ imports/ui/containers/BuildProfileSelectPage/BuildProfileSelectContainer.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import RaisedButton from 'material-ui/RaisedButton';
-
 import styles from './styles.css';
-
-
-const splashButtonFlexStyles ={
-  display: "flex",
-  flexFlow: "column nowrap",
-  justifyContent: "space-around",
-}
 
 class BuildProfileSelectContainer extends Component {
   constructor() {
@@ -25,18 +16,16 @@ class BuildProfileSelectContainer extends Component {
         <h2>Create Your Profile</h2>
 
         <div className='splash-buttons-container'>
-          <RaisedButton
+          <button
             className='splash-button'
-            style={splashButtonFlexStyles}
           >
             <span>Create Profile Using Linkedin</span>
-          </RaisedButton>
-          <RaisedButton
+          </button>
+          <button
             className='splash-button'
-            style={splashButtonFlexStyles}
           >
             <span>Create Profile From Scratch</span>
-          </RaisedButton>
+          </button>
         </div>
       </div>
     );
