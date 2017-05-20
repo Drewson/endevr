a44d935d870e7ebaa6dc6d3e7dc919df20f926b0
andrew.nelsonrd@gmail.com <andrew.nelsonrd@gmail.com>
2017-05-20 09:47:01 -0700

    changed some styles on user

--- imports/ui/components/User/user.js
+++ imports/ui/components/User/user.js
@@ -5,26 +5,38 @@ import FlatButton from 'material-ui/FlatButton';
 import Chip from 'material-ui/Chip';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import Avatar from 'material-ui/Avatar';
+import './style.css';
 
 import { Link } from 'react-router-dom';
 
 const User = ({ profile }) => {
     return (
-        <Link to={`/users/${profile._id}`}>
-            <Card>
-                <CardTitle title={profile.name} />
-                <CardHeader
-                    title="Test"
-                />
-                <p>{profile.bio}</p>
-                <p>{profile.skills}</p>
-                <p>{profile.email}</p>
-                <p>{profile.location}</p>
-                <p>{profile.sociallinks}</p>
-                <img src={profile.imageupload} style={{maxWidth:'450px', maxHeight:'450px'}} />
-                <FloatingActionButton>
-                    <ContentAdd />
-                </FloatingActionButton>
+        <Link to={`/users/${profile._id}`} style={{textDecoration:'none'}}>
+            <Card className='user' containerStyle={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                <div className='userLeft'>
+                    <Avatar src={profile.imageupload} size={200} />
+                    <CardTitle title={profile.name} />
+                    <CardHeader
+                        title={profile.email}
+                    />
+                    <p>Location: {profile.location}</p>
+                </div>
+                <div className='userCenter'>
+                    <p>{profile.bio}</p>
+                    <p>Skills: </p>
+                    <ul>
+                        {
+                            profile.skills.map(skill => <li>{skill}</li>)
+                        }
+                    </ul>
+                    <p>{profile.sociallinks}</p>
+                </div>
+                <div className='userRight'>
+                    <FloatingActionButton className='addUser'>
+                        <ContentAdd />
+                    </FloatingActionButton>
+                </div>
             </Card>
         </Link>
     )
